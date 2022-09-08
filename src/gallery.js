301eb4e9abdc2249428afe678e010e339ff24395
Ashley Davis <ashley@codecapers.com.au>
2022-09-09 09:14:40 +1000

    Refactor pass values through to the gallery component as props.

--- src/gallery.js
+++ src/gallery.js
@@ -1,58 +1,11 @@
 import { createLayout } from "./layout";
 
 //
-// Uncomment this for the full gallery test data.
-//
-let gallery = require("./gallery.json");
-
-gallery = gallery.map(item => {
-    return {
-        thumb: item.urls.thumb,
-        width: item.width,
-        height: item.height,
-    };
-});
-
-//
-// Uncomment this for a single item in the gallery.
-//
-// const gallery = [
-//     {
-//         thumb: "https://via.placeholder.com/140x100",
-//         width: 140,
-//         height: 100,
-//     },
-// ];
-
-//
-// Uncomment this for three items in the gallery.
-//
-// const gallery = [
-//     {
-//         thumb: "https://via.placeholder.com/240x200",
-//         width: 240,
-//         height: 200,
-//     },
-//     {
-//         thumb: "https://via.placeholder.com/220x200",
-//         width: 220,
-//         height: 200,
-//     },
-//     {
-//         thumb: "https://via.placeholder.com/230x200",
-//         width: 230,
-//         height: 200,
-//     },
-// ];
-
-//
 // A photo gallery component.
 //
-export function Gallery() {
+export function Gallery({ items, galleryWidth, targetRowHeight }) {
 
-    const galleryWidth = 600;
-    const targetRowHeight = 200;
-    const rows = createLayout(gallery, galleryWidth, targetRowHeight);
+    const rows = createLayout(items, galleryWidth, targetRowHeight);
 
     return (
         <div
