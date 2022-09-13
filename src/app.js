c720f2e5a0c26f80e7811c32c4756125340498e9
Ashley Davis <ashley@codecapers.com.au>
2022-09-14 09:14:16 +1000

    Refactor to extract the gallery to it's one page.

--- src/app.js
+++ src/app.js
@@ -1,53 +1,5 @@
-import { Gallery } from "./gallery";
-import axios from "axios";
 import React from "react";
-
-const BASE_URL = "http://localhost:3000";
-
-//
-// Uncomment this for the full gallery test data.
-//
-// let gallery = require("./gallery.json");
-
-// gallery = gallery.map(item => {
-//     return {
-//         thumb: item.urls.thumb,
-//         width: item.width,
-//         height: item.height,
-//     };
-// });
-
-//
-// Uncomment this for a single item in the gallery.
-//
-// const gallery = [
-//     {
-//         thumb: "https://via.placeholder.com/140x100",
-//         width: 140,
-//         height: 100,
-//     },
-// ];
-
-//
-// Uncomment this for three items in the gallery.
-//
-// const gallery = [
-//     {
-//         thumb: "https://via.placeholder.com/240x200",
-//         width: 240,
-//         height: 200,
-//     },
-//     {
-//         thumb: "https://via.placeholder.com/220x200",
-//         width: 220,
-//         height: 200,
-//     },
-//     {
-//         thumb: "https://via.placeholder.com/230x200",
-//         width: 230,
-//         height: 200,
-//     },
-// ];
+import { GalleryPage } from "./gallery-page";
 
 export class App extends React.Component {
 
@@ -57,22 +9,9 @@ export class App extends React.Component {
         this.state = {};
     }
 
-    async componentDidMount() {
-
-        const response = await axios.get(`${BASE_URL}/assets`);
-
-        this.setState({
-            items: response.data.assets,
-        });
-    }
-
     render() {
         return (
-            <Gallery 
-                galleryWidth={600}
-                targetRowHeight={200}
-                items={this.state.items}
-                />
+            <GalleryPage />
         );
     }
 }
---
+++ src/gallery-page.js
@@ -0,0 +1,78 @@
+import { Gallery } from "./gallery";
+import axios from "axios";
+import React from "react";
+
+const BASE_URL = "http://localhost:3000";
+
+//
+// Uncomment this for the full gallery test data.
+//
+// let gallery = require("./gallery.json");
+
+// gallery = gallery.map(item => {
+//     return {
+//         thumb: item.urls.thumb,
+//         width: item.width,
+//         height: item.height,
+//     };
+// });
+
+//
+// Uncomment this for a single item in the gallery.
+//
+// const gallery = [
+//     {
+//         thumb: "https://via.placeholder.com/140x100",
+//         width: 140,
+//         height: 100,
+//     },
+// ];
+
+//
+// Uncomment this for three items in the gallery.
+//
+// const gallery = [
+//     {
+//         thumb: "https://via.placeholder.com/240x200",
+//         width: 240,
+//         height: 200,
+//     },
+//     {
+//         thumb: "https://via.placeholder.com/220x200",
+//         width: 220,
+//         height: 200,
+//     },
+//     {
+//         thumb: "https://via.placeholder.com/230x200",
+//         width: 230,
+//         height: 200,
+//     },
+// ];
+
+export class GalleryPage extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+
+    async componentDidMount() {
+
+        const response = await axios.get(`${BASE_URL}/assets`);
+
+        this.setState({
+            items: response.data.assets,
+        });
+    }
+
+    render() {
+        return (
+            <Gallery 
+                galleryWidth={600}
+                targetRowHeight={200}
+                items={this.state.items}
+                />
+        );
+    }
+}
