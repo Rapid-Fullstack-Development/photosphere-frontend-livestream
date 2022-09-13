f770bf06f2e3646dfec8b4afd4f0a6d125a2c6af
Ashley Davis <ashley@codecapers.com.au>
2022-09-14 09:24:55 +1000

    Added a very simple and very ugly header to change between main and upload pages.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { GalleryPage } from "./gallery-page";
 import { UploadPage } from "./upload-page";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
 export class App extends React.Component {
 
@@ -14,6 +14,11 @@ export class App extends React.Component {
     render() {
         return (
             <BrowserRouter>
+                <div>
+                    <Link to="/">Home</Link>
+                    &nbsp;|&nbsp;
+                    <Link to="/upload">Upload</Link>
+                </div>
                 <Routes>
                     <Route path="/" element={<GalleryPage />} />
                     <Route path="/upload" element={<UploadPage />} />
