121819024f8310db0a7dd52755375e19fa1e5084
Ashley Davis <ashley@codecapers.com.au>
2022-09-14 09:44:05 +1000

    Can now upload photos using the new upload page.

--- src/upload-page.js
+++ src/upload-page.js
@@ -1,11 +1,61 @@
+import axios from "axios";
 import React from "react";
 
+const BASE_URL = "http://localhost:3000";
+
 export class UploadPage extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+
+    onFilesChanged = files => {
+        this.setState({
+            files: Array.from(files),
+        });
+    };
+
+    onUploadFiles = async () => {
+        for (const file of this.state.files) {
+            console.log(`Upload file ${file.name}...`);
+            await axios.post(`${BASE_URL}/asset`, file, {
+                headers: {
+                    "file-name": file.name,
+                    "content-type": file.type,
+                    "width": 255,
+                    "height": 255,
+                },
+            });
+            console.log(`...uploaded file ${file.name}.`);
+        }
+    };
 
     render() {
         return (
             <div>
-                Hello!
+                <p>Click the button and choose files to upload</p>
+                <input
+                    type="file"
+                    multiple={true}
+                    onChange={evt => this.onFilesChanged(evt.target.files)}
+                    />
+
+                {this.state.files !== undefined
+                    && <div>
+                        <h2>File to upload:</h2>
+                        {this.state.files.map(file => {
+                            return (
+                                <div key={file.name}>
+                                    {file.name}
+                                </div>
+                            )
+                        })}
+
+                        <button onClick={this.onUploadFiles}>Upload</button>
+                    </div>
+                }
             </div>
         );
     }
